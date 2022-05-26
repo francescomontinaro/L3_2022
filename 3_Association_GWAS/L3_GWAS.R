d448bfa22c0908f68ea6c4d75a68108d188b6bd2
francesco <francesco.montinaro@gmail.com>
2022-05-26 09:10:18 +0200

    R  checked

--- 3_Association_GWAS/L3_GWAS.R
+++ 3_Association_GWAS/L3_GWAS.R
@@ -1,13 +1,14 @@
+setwd("../3_Association_GWAS/")
 library(qqman)
-system("plink --bfile HapMap_3_r3_13 --assoc --out assoc_results")
+system("./plink --bfile HapMap_3_r3_13 --assoc --out assoc_results")
 
 
-system("plink --bfile HapMap_3_r3_13 --covar covar_mds.txt --logistic --hide-covar --out logistic_results")
+system("./plink --bfile HapMap_3_r3_13 --covar covar_mds.txt --logistic --hide-covar --out logistic_results")
 
 
 system("awk '!/'NA'/' logistic_results.assoc.logistic > logistic_results.assoc_2.logistic")
 
-system("plink --bfile HapMap_3_r3_13 -assoc --adjust --out adjusted_assoc_results")
+system("./plink --bfile HapMap_3_r3_13 -assoc --adjust --out adjusted_assoc_results")
 
 results_log <- read.table("logistic_results.assoc_2.logistic", head=TRUE)
 jpeg("Logistic_manhattan.jpeg")
@@ -15,18 +16,13 @@ manhattan(results_log,chr="CHR",bp="BP",p="P",snp="SNP", main = "Manhattan plot:
 dev.off()
 
 results_as <- read.table("assoc_results.assoc", head=TRUE)
-jpeg("assoc_manhattan.jpeg")
+
 manhattan(results_as,chr="CHR",bp="BP",p="P",snp="SNP", main = "Manhattan plot: assoc")
-dev.off()  
 
 
-jpeg("QQ-Plot_logistic.jpeg")
 qq(results_log$P, main = "Q-Q plot of GWAS p-values : log")
-dev.off()
 
 results_as <- read.table("assoc_results.assoc", head=TRUE)
-jpeg("QQ-Plot_assoc.jpeg")
 qq(results_as$P, main = "Q-Q plot of GWAS p-values : log")
-dev.off()
 
 
